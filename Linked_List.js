a860369029087e5d3c06ccc61e875b871bff2849
designed2code <destined2code@gmail.com>
2021-09-08 22:10:08 +0530

    Create Linked List class

--- Linked_List.js
+++ Linked_List.js
@@ -8,9 +8,19 @@ this.next = null
 }
 }
 
-
-
-
+// Create a LL class
+class LinkedList{
+  // Add a constructor function
+  constructor(value){
+  // Create a newNode
+  const newNode = new Node(value)
+  // Point the newNode to head and tail
+  this.head = newNode
+  this.tail = this.head
+  // Keep track of length
+  this.length = 1
+  }
+}
 
 
 
