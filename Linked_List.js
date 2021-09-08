b887a527589636b1635464511fd5ac2fb629ebc5
designed2code <destined2code@gmail.com>
2021-09-08 22:07:09 +0530

    Create Node in Linked List

---
+++ Linked_List.js
@@ -0,0 +1,19 @@
+// Create a Node class
+class Node {
+// Add a constructor function
+constructor(value){
+// Initialize the value and pointer
+this.value = value
+this.next = null
+}
+}
+
+
+
+
+
+
+
+
+
+
